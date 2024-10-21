c4c6748314f2694438577c2438af1414fa62b35a
ljlfct01 <118674843+ljlfct01@users.noreply.github.com>
2024-10-22 03:26:29 +0800

    Update list.php

--- list.php
+++ list.php
@@ -17,15 +17,7 @@
   "ads": [],
   "sites": [
 
- {
-      "key": "豆豆",
-      "name": "豆瓣推荐┃肖玉nota ys",
-      "type": 3,
-      "api": "csp_DouDou",
-      "searchable": 0,
-      "quickSearch": 0,
-      "filterable": 0
-    },
+ 
    
     {
       "key": "📺 电视直播 | JAR",
