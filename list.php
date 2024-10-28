ca08d1b8ce5057bef7ca363ed867601a344e749e
ljlfct01 <118674843+ljlfct01@users.noreply.github.com>
2024-10-28 08:00:05 +0800

    Update list.php

--- list.php
+++ list.php
@@ -4,23 +4,7 @@
   "app_message": "本「APP」以及「时光机」均为开源免费项目, 仅供测试学习交流~\n\n使用过程中若出现广告均为三方插入, 与本公众号无关, 请勿相信, 配置 jxtoken 过滤这些广告~\n\n关注公众号加「微信群」可获取更多信息~",
   "jar_message": "回复“签到”, 领临时密钥。或者加入「微信群」领取超级密钥, 长期有效!",
   "filter": [
-    "王二小放牛娃.xyz",
-    "神秘的哥哥们",
-    "|",
-    "｜",
-    "防失联",
-    "关注",
-    "【",
-    "】",
-    "┃ ",
-    "小米接口群：665981772 "
+  
   ],
-  "prefix": "★公瑾TV★",
-  "prefix_wolong": "★卧龙TV★",
-  "title": "公众号「码上瞧瞧」",
-  "picture": "https://www.lintech.work/static/img/%E6%8F%92%E5%85%9C.png",
-  "jxUrl": "https://www.lintech.work/api/m3u8/parse?token=%s&url=%s",
-  "notice": "https://www.lintech.work/static/config/notice.json",
-  "live": "https://live.fanmingming.com/tv/m3u/ipv6.m3u",
-  "upgrade": "https://ghproxy.liuzhicong.com/https://raw.githubusercontent.com/bestpvp/config/main/"
+
 }
