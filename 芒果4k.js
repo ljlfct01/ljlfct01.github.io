116818d324ba2a7fdc6b1ce2b5d15fef06c16c0e
ljlfct01 <118674843+ljlfct01@users.noreply.github.com>
2025-07-27 16:13:20 +0800

    Update 芒果4k.js

--- 芒果4k.js
+++ 芒果4k.js
@@ -664,7 +664,7 @@ var rule = {
     play_parse: true,
     lazy: $js.toString(() => {
         try {
-            let api = "" + input.split("?")[0];
+            let api = "http://zhuimi.xn--v4q818bf34b.com/moyu/zhuimi?token=z2UWYdw6&url=" + input.split("?")[0];
             console.log(api);
             let response = fetch(api, {
                 method: 'get',
