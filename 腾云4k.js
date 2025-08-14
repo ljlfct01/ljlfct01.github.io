c7429603da79c64c0050336778349f8100060a32
ljlfct01 <118674843+ljlfct01@users.noreply.github.com>
2025-08-15 01:48:27 +0800

    Update 腾云4k.js

--- 腾云4k.js
+++ 腾云4k.js
@@ -158,7 +158,7 @@ var rule = {
         let zp = d.filter(function(it) {
             return !(it.type && it.type !== "正片")
         });
-        VOD.vod_play_from = yg.length < 1 ? "老三4K" : "老三4KT$$$预告及花絮";
+        VOD.vod_play_from = yg.length < 1 ? "自用4K" : "自用4KT$$$预告及花絮";
         VOD.vod_play_url = yg.length < 1 ? d.map(function(it) {
             return it.title + "$" + it.url
         }).join("#") : [zp, yg].map(function(it) {
