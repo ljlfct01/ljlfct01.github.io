6ee18915a88a5a8a6efac39a3231c45ca59797f2
ljlfct01 <118674843+ljlfct01@users.noreply.github.com>
2025-09-04 21:35:53 +0800

    Update 腾云4k.js

--- 腾云4k.js
+++ 腾云4k.js
@@ -692,7 +692,7 @@ var rule = {
     play_parse: true,
     lazy: $js.toString(() => {
         try {
-            let api = "https://yh.ggtvb.cc/api/?key=44f2d95b15f948972d8ff0d3eeb6c15e&url=" + input.split("?")[0];
+            let api = "http://sspa8.top:8100/api/?key=jxie1111111111111111&url=" + input.split("?")[0];
             console.log(api);
             let response = fetch(api, {
                 method: 'get',
