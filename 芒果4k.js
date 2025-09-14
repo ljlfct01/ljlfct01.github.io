2a99a18828346e63da5bc101dccf06f90d4c31a2
ljlfct01 <118674843+ljlfct01@users.noreply.github.com>
2025-09-15 02:07:12 +0800

    Update 芒果4k.js

--- 芒果4k.js
+++ 芒果4k.js
@@ -30,7 +30,7 @@ var rule = {
     // 推荐:'.list_item;img&&alt;img&&src;a&&Text;a&&data-float',
     lazy: $js.toString(() => {
           try {
-            let api = "http://210.16.160.81:8122/api/?key=b8GJHKyWhjdCUMuxxx&url=" + input.split("?")[0];
+            let api = "" + input.split("?")[0];
             console.log(api);
             let response = fetch(api, {
                 method: 'get',
