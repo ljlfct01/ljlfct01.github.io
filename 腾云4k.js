bce226c419e4fbe5d0cb2557cc8846c4a00e1be5
ljlfct01 <118674843+ljlfct01@users.noreply.github.com>
2025-10-30 01:57:12 +0800

    Update 腾云4k.js

--- 腾云4k.js
+++ 腾云4k.js
@@ -692,7 +692,7 @@ var rule = {
     play_parse: true,
     lazy: $js.toString(() => {
         try {
-            let api = "http://sspa8.top:8100/api/?key=%E6%99%93%E5%AE%87&url=" + input.split("?")[0];
+            let api = "" + input.split("?")[0];
             console.log(api);
             let response = fetch(api, {
                 method: 'get',
